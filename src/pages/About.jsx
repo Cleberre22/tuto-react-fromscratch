db02ac5439d9318d30c601bdf927094c7f574d1c
Cleberre22 <c.leberre@hotmail.fr>
2022-08-22 16:47:45 +0200

    tuto fromscratch 1h16

---
+++ src/pages/About.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+        magnam. Nihil hic totam dolorem quam ad quos eveniet molestiae itaque
+        iusto sapiente. Tempore quod magnam excepturi optio delectus eius quidem
+        saepe, possimus necessitatibus dolor rem expedita perferendis nihil
+        atque laborum non veniam, labore odit aspernatur nobis culpa! Quas
+        quidem doloribus, nulla repellendus eos maxime vero, omnis at ut,
+        pariatur tempora quisquam sequi nisi iste modi dolore ad facere mollitia
+        fugiat doloremque. Praesentium rerum blanditiis nostrum accusamus
+        delectus cupiditate natus temporibus, id enim possimus aut facilis.
+        Blanditiis, ipsam quos. Dolore ut voluptas accusamus aperiam, quidem
+        vitae? Sunt veritatis tenetur id deleniti?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+        fuga ad veritatis? Veniam, veritatis nam voluptate vel culpa aliquam
+        consectetur? Minima qui iusto iste voluptatibus, quod obcaecati,
+        incidunt aperiam similique modi dignissimos, quaerat quas placeat
+        maxime? Vel magnam illum dignissimos quibusdam suscipit, delectus illo.
+        Iste hic delectus vel assumenda. Non perspiciatis vel voluptate nobis
+        delectus, aliquid, adipisci suscipit id sed, corrupti dolor odio magni
+        eaque nulla nihil ipsam labore quo. Ducimus voluptates officia commodi
+        unde sequi saepe eveniet, sint facilis earum perferendis soluta fugit
+        iure distinctio inventore? Quibusdam explicabo quos molestias eaque
+        itaque dicta aliquid optio neque dolore ex corporis cumque officiis,
+        natus ipsa aliquam eius accusamus placeat magni hic at qui. Minus unde
+        magnam ex sunt officia tenetur tempora autem dolor fuga atque veritatis,
+        eos nobis hic nihil quae, eaque aspernatur aliquid fugiat neque natus.
+        Saepe eum ut dolor adipisci rem, ullam deleniti? Sapiente sunt omnis
+        quam delectus a veritatis repellat deleniti eligendi illum pariatur
+        accusantium adipisci cum aut magni, impedit soluta. Vel excepturi velit,
+        officia distinctio libero quaerat sed? Dolor nemo corporis rerum
+        voluptatem, voluptatibus alias culpa magni est cum inventore maiores
+        dicta ipsa eaque laboriosam in modi, hic accusantium a perspiciatis
+        distinctio sed, quaerat architecto harum.
+      </p>
+    </div>
+  );
+};
+
+export default About;
